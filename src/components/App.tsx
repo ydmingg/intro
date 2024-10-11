20e350a86a4b6ef85063b1057d6e0051df3fc17e
ydming <xm95zz@gmail.com>
2024-10-11 17:39:47 +0800

    feat card

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,19 +5,9 @@ function App() {
     return (
         <>
             <div className="box-img">
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src="/vite.svg" className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src="/src/assets/react.svg" className="logo react" alt="React logo" />
-                </a>
+                <div className="">{count}</div>
+                <button onClick={()=>setCount(count => count+1) }>增加数量</button>
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-                <p>Edit <code>src/App.tsx</code> and save to test HMR</p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
         </>
     )
 }

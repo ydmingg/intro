3ec27cf47196016fc08edeed466bcd95286c5876
ydmingWin <ydminggg@gmail.com>
2024-10-12 23:44:21 +0800

    fix home

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { createRoot } from 'react-dom/client'
 import App from './components/App'
 
 // 导入css
-import '../css/index.css'
-import './index.css'
+import './styles/base.css'
+import './styles/index.css'
 
 // 渲染组件
 const root = createRoot(document.getElementById('root')!)

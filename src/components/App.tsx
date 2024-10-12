f85a70969c3b865ab8e99f41f958138e0ec52eb0
ydming <xm95zz@gmail.com>
2024-10-12 17:15:48 +0800

    feat components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import { Button } from "@/components/ui/button";
+// import { useState } from 'react'
+// import { Button } from "@/components/ui/button";
 import { OrbitingCirclesDemo } from "@/components/home"
 
 function App() {
-    const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
     return (
         <>
-            <div className="box-img">
+            {/* <div className="box-img">
                 <div className="">{count}</div>
             </div>
             <Button
                 className='mb-12'
                 onClick={() => setCount(count => count + 1)}
-            >增加数量</Button>
+            >增加数量</Button> */}
             <OrbitingCirclesDemo></OrbitingCirclesDemo>
         </>
     )

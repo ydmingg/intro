d0415c4dcd358e14c7f908a29af2751d71593bd3
ydming <xm95zz@gmail.com>
2024-10-12 18:01:18 +0800

    feat components

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,26 @@
 import path from "path";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import pkg from "./package.json";
+// import pkg from "./package.json";
 
 export default defineConfig({
     root: '', // 设置项目根目录
     base: './', // 设置公共基础路径
     plugins: [react()],
-    build: {
-        minify: "esbuild", // 压缩代码
-        rollupOptions: {
-            input: {   // 入口文件
-                "intro": "src/main.tsx",
-            },
-            output: {
-                dir: "dist", // 输出目录
-                format: "es",  // 打包文件格式
-                entryFileNames: `[name]-${(pkg.version as any).replaceAll('.', '')}.js`, // 输出文件名称
-            }
-        }
-    },
+    // build: {
+    //     minify: "esbuild", // 压缩代码
+    //     rollupOptions: {
+    //         input: {   // 入口文件
+    //             "intro": "src/main.tsx",
+    //             "intro-css": "src/index.css",
+    //         },
+    //         output: {
+    //             dir: "dist", // 输出目录
+    //             format: "es",  // 打包文件格式
+    //             entryFileNames: `[name]-${(pkg.version as any).replaceAll('.', '')}.js`, // 输出文件名称
+    //         }
+    //     }
+    // },
     server: {
         host: '0.0.0.0',
         port: 8065,

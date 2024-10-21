0abd9af28c52e70b0fa363fd75e612d5dc3f4849
ydming <xm95zz@gmail.com>
2024-10-21 17:04:25 +0800

    feat animate2

--- src/components/other-bg.tsx
+++ src/components/other-bg.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useRef } from 'react'
+let direction = 1; // 1表示向右，-1表示向左
+let position = 0;
 
 export function OtherBg() { 
     // 设置滚动鼠标时，图片修改transform的translate属性值
@@ -8,20 +10,28 @@ export function OtherBg() {
     useEffect(() => {
         if (oMyBg.current) { 
             const rect = oMyBg.current.getBoundingClientRect(); // 获取元素相对于视口的位置信息
-            const scrollX = window.scrollX;
-            const scrollY = window.scrollY;
 
-            // 计算新的位置
-            let newX = scrollX + (window.innerWidth - rect.width) / 2;
-            let newY = scrollY + (window.innerHeight - rect.height) / 2;
+            function animate() { 
+                position += direction * 10; // 每次移动2个像素
+                console.log("移动了：",-position);
+                oMyBg.current!.style.transform = `translateX(${-position}px)`; // 通过transform移动小球
+                
+                
+                
+                // 如果小球到达边界，反转方向
+                if (position >= window.innerWidth - rect.width - 128 || position < 0) {
+                    direction *= -1; // 反转方向
+                    // direction = 0
+                
+                }
 
-            // 确保元素不会超出视口边界
-            if (newX < 0) newX = 0;
-            if (newY < 0) newY = 0;
-            if (newX + rect.width > window.innerWidth) newX = window.innerWidth - rect.width;
-            if (newY + rect.height > window.innerHeight) newY = window.innerHeight - rect.height;
+                
+                
+                requestAnimationFrame(animate);
+            }
 
-            oMyBg.current.style.transform = `translate(${newX}px, ${newY}px)`;
+            animate();
+
         }
 
     }, []);
@@ -29,7 +39,8 @@ export function OtherBg() {
     return (
         <>
             <div ref={oMyBg} className="absolute top-32 right-32 z-0" style={{
-                transition: 'all 0.5s ease',
+                display: 'flex',
+                // transition: 'all 0.3s ease',
                 // transform: `translate3d(${position.x}px, ${position.y}px, 0)`,
             }}>
                 <img loading="lazy" width="525" src="https://www.qzq.at/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fwhite-circle.e1e5dc56.png&w=640&q=75" alt="" />

c651688ced62b0658ac78d6bc4fc778cb9395874
ydming <xm95zz@gmail.com>
2024-11-21 17:56:45 +0800

    update bg

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -2,57 +2,57 @@ import OrbitingCircles from "@/components/ui/orbiting-circles";
 
 export function OrbitingCirclesDemo() {
   return (
-    <section className="w-full px-4 lg:px-16 xl:px-32 2xl:px-44 relative z-10 my-12 md:mt-40 md:mb-12 py-24 flex flex-wrap-reverse md:flex-nowrap justify-items-center justify-around items-center gap-16">
-          <div className="">asdfasdf</div>
-          
-        {/* Inner Circles */}
-        <OrbitingCircles
-            className="size-[30px] border-none bg-transparent"
-            duration={20}
-            delay={20}
-            radius={80}
-        >
-            <Icons.whatsapp />
-        </OrbitingCircles>
-        <OrbitingCircles
-            className="size-[30px] border-none bg-transparent"
-            duration={20}
-            delay={10}
-            radius={80}
-        >
-            <Icons.notion />
-        </OrbitingCircles>
+    <div className="w-full px-4 lg:px-16 xl:px-32 2xl:px-44 relative z-10 my-12 md:mt-40 md:mb-12 py-24 flex flex-wrap-reverse md:flex-nowrap justify-items-center justify-around items-center gap-16">
+      {/* <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-black">
+        
+      </span> */}
 
-        {/* Outer Circles (reverse) */}
-        <OrbitingCircles
-            className="size-[192px] border-none bg-transparent"
-            radius={190}
-            duration={20}
-            reverse
-        >
-            <Icons.googleDrive />
-        </OrbitingCircles>
-        <OrbitingCircles
-            className="size-[50px] border-none bg-transparent"
-            radius={190}
-            duration={20}
-            delay={20}
-            reverse
-        >
-            <Icons.gitHub />
-        </OrbitingCircles>
-    </section>
+      {/* Inner Circles */}
+      <OrbitingCircles
+        className="size-12 border-none bg-transparent"
+        duration={10}
+        delay={20}
+        radius={210}
+      >
+        <Icons.whatsapp />
+      </OrbitingCircles>
+      <OrbitingCircles
+        className="size-8 border-none bg-transparent"
+        duration={16}
+        delay={10}
+        radius={210}
+      >
+        <Icons.notion />
+      </OrbitingCircles>
+
+      {/* Outer Circles (reverse) */}
+      <OrbitingCircles
+        className="size-[52px] border-none bg-transparent"
+        radius={260}
+        duration={20}
+        reverse
+      >
+        <Icons.googleDrive />
+      </OrbitingCircles>
+      <OrbitingCircles
+        className="size-48 border-none bg-transparent"
+        radius={260}
+        duration={9}
+        delay={20}
+        reverse
+      >
+        <Icons.gitHub />
+      </OrbitingCircles>
+    </div>
   );
 }
 
 const Icons = {
   gitHub: () => (
-    <svg width="100" height="100" viewBox="0 0 438.549 438.549">
-      <path
-        fill="currentColor"
-        d="M409.132 114.573c-19.608-33.596-46.205-60.194-79.798-79.8-33.598-19.607-70.277-29.408-110.063-29.408-39.781 0-76.472 9.804-110.063 29.408-33.596 19.605-60.192 46.204-79.8 79.8C9.803 148.168 0 184.854 0 224.63c0 47.78 13.94 90.745 41.827 128.906 27.884 38.164 63.906 64.572 108.063 79.227 5.14.954 8.945.283 11.419-1.996 2.475-2.282 3.711-5.14 3.711-8.562 0-.571-.049-5.708-.144-15.417a2549.81 2549.81 0 01-.144-25.406l-6.567 1.136c-4.187.767-9.469 1.092-15.846 1-6.374-.089-12.991-.757-19.842-1.999-6.854-1.231-13.229-4.086-19.13-8.559-5.898-4.473-10.085-10.328-12.56-17.556l-2.855-6.57c-1.903-4.374-4.899-9.233-8.992-14.559-4.093-5.331-8.232-8.945-12.419-10.848l-1.999-1.431c-1.332-.951-2.568-2.098-3.711-3.429-1.142-1.331-1.997-2.663-2.568-3.997-.572-1.335-.098-2.43 1.427-3.289 1.525-.859 4.281-1.276 8.28-1.276l5.708.853c3.807.763 8.516 3.042 14.133 6.851 5.614 3.806 10.229 8.754 13.846 14.842 4.38 7.806 9.657 13.754 15.846 17.847 6.184 4.093 12.419 6.136 18.699 6.136 6.28 0 11.704-.476 16.274-1.423 4.565-.952 8.848-2.383 12.847-4.285 1.713-12.758 6.377-22.559 13.988-29.41-10.848-1.14-20.601-2.857-29.264-5.14-8.658-2.286-17.605-5.996-26.835-11.14-9.235-5.137-16.896-11.516-22.985-19.126-6.09-7.614-11.088-17.61-14.987-29.979-3.901-12.374-5.852-26.648-5.852-42.826 0-23.035 7.52-42.637 22.557-58.817-7.044-17.318-6.379-36.732 1.997-58.24 5.52-1.715 13.706-.428 24.554 3.853 10.85 4.283 18.794 7.952 23.84 10.994 5.046 3.041 9.089 5.618 12.135 7.708 17.705-4.947 35.976-7.421 54.818-7.421s37.117 2.474 54.823 7.421l10.849-6.849c7.419-4.57 16.18-8.758 26.262-12.565 10.088-3.805 17.802-4.853 23.134-3.138 8.562 21.509 9.325 40.922 2.279 58.24 15.036 16.18 22.559 35.787 22.559 58.817 0 16.178-1.958 30.497-5.853 42.966-3.9 12.471-8.941 22.457-15.125 29.979-6.191 7.521-13.901 13.85-23.131 18.986-9.232 5.14-18.182 8.85-26.84 11.136-8.662 2.286-18.415 4.004-29.263 5.146 9.894 8.562 14.842 22.077 14.842 40.539v60.237c0 3.422 1.19 6.279 3.572 8.562 2.379 2.279 6.136 2.95 11.276 1.995 44.163-14.653 80.185-41.062 108.068-79.226 27.88-38.161 41.825-81.126 41.825-128.906-.01-39.771-9.818-76.454-29.414-110.049z"
-      />
-    </svg>
+    <img src="https://book.funxdata.com/public/img/website/ydm.png"
+        className="relative mr-16 mt-24 shadow-lg rounded-full"
+        alt=""
+    />
   ),
   notion: () => (
     <svg
@@ -85,38 +85,37 @@ const Icons = {
     </svg>
   ),
   googleDrive: () => (
-    // <svg
-    //   width="100"
-    //   height="100"
-    //   viewBox="0 0 87.3 78"
-    //   xmlns="http://www.w3.org/2000/svg"
-    // >
-    //   <path
-    //     d="m6.6 66.85 3.85 6.65c.8 1.4 1.95 2.5 3.3 3.3l13.75-23.8h-27.5c0 1.55.4 3.1 1.2 4.5z"
-    //     fill="#0066da"
-    //   />
-    //   <path
-    //     d="m43.65 25-13.75-23.8c-1.35.8-2.5 1.9-3.3 3.3l-25.4 44a9.06 9.06 0 0 0 -1.2 4.5h27.5z"
-    //     fill="#00ac47"
-    //   />
-    //   <path
-    //     d="m73.55 76.8c1.35-.8 2.5-1.9 3.3-3.3l1.6-2.75 7.65-13.25c.8-1.4 1.2-2.95 1.2-4.5h-27.502l5.852 11.5z"
-    //     fill="#ea4335"
-    //   />
-    //   <path
-    //     d="m43.65 25 13.75-23.8c-1.35-.8-2.9-1.2-4.5-1.2h-18.5c-1.6 0-3.15.45-4.5 1.2z"
-    //     fill="#00832d"
-    //   />
-    //   <path
-    //     d="m59.8 53h-32.3l-13.75 23.8c1.35.8 2.9 1.2 4.5 1.2h50.8c1.6 0 3.15-.45 4.5-1.2z"
-    //     fill="#2684fc"
-    //   />
-    //   <path
-    //     d="m73.4 26.5-12.7-22c-.8-1.4-1.95-2.5-3.3-3.3l-13.75 23.8 16.15 28h27.45c0-1.55-.4-3.1-1.2-4.5z"
-    //     fill="#ffba00"
-    //   />
-      // </svg>
-    <img src="https://book.funxdata.com/public/img/website/ydm.png" className="relative mr-16 mt-24 shadow-lg rounded-full" alt="" />
+    <svg
+      width="100"
+      height="100"
+      viewBox="0 0 87.3 78"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="m6.6 66.85 3.85 6.65c.8 1.4 1.95 2.5 3.3 3.3l13.75-23.8h-27.5c0 1.55.4 3.1 1.2 4.5z"
+        fill="#0066da"
+      />
+      <path
+        d="m43.65 25-13.75-23.8c-1.35.8-2.5 1.9-3.3 3.3l-25.4 44a9.06 9.06 0 0 0 -1.2 4.5h27.5z"
+        fill="#00ac47"
+      />
+      <path
+        d="m73.55 76.8c1.35-.8 2.5-1.9 3.3-3.3l1.6-2.75 7.65-13.25c.8-1.4 1.2-2.95 1.2-4.5h-27.502l5.852 11.5z"
+        fill="#ea4335"
+      />
+      <path
+        d="m43.65 25 13.75-23.8c-1.35-.8-2.9-1.2-4.5-1.2h-18.5c-1.6 0-3.15.45-4.5 1.2z"
+        fill="#00832d"
+      />
+      <path
+        d="m59.8 53h-32.3l-13.75 23.8c1.35.8 2.9 1.2 4.5 1.2h50.8c1.6 0 3.15-.45 4.5-1.2z"
+        fill="#2684fc"
+      />
+      <path
+        d="m73.4 26.5-12.7-22c-.8-1.4-1.95-2.5-3.3-3.3l-13.75 23.8 16.15 28h27.45c0-1.55-.4-3.1-1.2-4.5z"
+        fill="#ffba00"
+      />
+    </svg>
   ),
   whatsapp: () => (
     <svg

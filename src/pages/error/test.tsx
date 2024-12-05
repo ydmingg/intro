00c1610f74dda0e4a996163944a12334b930c959
ydming <xm95zz@gmail.com>
2024-12-05 17:57:09 +0800

    update init

--- src/pages/error/test.tsx
+++ src/pages/error/test.tsx
@@ -1,9 +1,32 @@
 export function Test() {
+    const arr = [9, 5, 6, 8, 2, 7, 3, 4, 1];
     
+    console.log(Math.floor(arr.length / 2));
+    // function ssssss(arr:any): any {
+    //     if (arr.length <= 1) { 
+    //         return arr; 
+    //     }
+    // 　　var pivotIndex = Math.floor(arr.length / 2);
+    //     var pivot = arr.splice(pivotIndex, 1)[0];
+        
+        
+    // 　　var left = [];
+    // 　　var right = [];
+    // 　　for (var i = 0; i < arr.length; i++) {
+    //     if (arr[i] < pivot) {
+    //         left.push(arr[i]);
+    //     } else {
+    //         right.push(arr[i]);
+    //     }
+    // }
+    //     // concat 拼接数组
+    //     return ssssss(left).concat([pivot], ssssss(right));
+    // };
+
+
+    // const aaaa = ssssss(arr);
+    // console.log(aaaa);
     
-
-
-
 
     return (
         <div>
